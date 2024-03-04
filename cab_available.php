d659dd88aea773cefe254a0bd19fc9220b26fe3c
saarah zainab <155457563+saarah-zainab@users.noreply.github.com>
2024-03-04 20:30:54 +0530

    Create Cabs Avialable Body

---
+++ cab_available.php
@@ -0,0 +1,25 @@
+<!-- Section-->
+<section class="py-5">
+    <div class="container px-4 px-lg-5 mt-5 card rounded-0 card-outline card-purple shadow">
+        <div class="row">
+            <div class="col-md-12">
+            <center>
+            <h1 class="display-4 fw-bolder">Available Cabs</h1>
+            <hr>
+            </center>
+                <div class="form-group">
+                <div class="input-group mb-3">
+                    <input type="search" id="search" class="form-control" placeholder="Search Here..." aria-label="Search Here" aria-describedby="basic-addon2">
+                    <div class="input-group-append">
+                        <span class="input-group-text bg-success" id="basic-addon2"><i class="fa fa-search"></i></span>
+                    </div>
+                </div>
+                <hr>
+                </div>
+                <div class="row gx-4 gx-lg-5 row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-xl-3" id="cab_list">
+                </div>
+                <div id="noResult" style="display:none" class="text-center"><b>No Results!!</b></div>
+            </div>
+        </div>
+    </div>
+</section>
