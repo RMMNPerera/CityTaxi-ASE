236efa2fb6552b6eeaacb9a60cf9270a3be7d3ab
charithanayana <charithanayana@gmail.com>
2024-03-05 07:14:31 +0530

    Styles css code included

---
+++ manage_account2.php
@@ -0,0 +1,119 @@
+<style>
+    #cimg{
+        width:15vw;
+        height:20vh;
+        object-fit:scale-down;
+        object-position:center center;
+    }
+</style>
+<div class="content py-5 mt-5">
+    <div class="container">
+        <div class="card card-outline card-purple shadow rounded-0">
+            <div class="card-header">
+                <h4 class="card-title"><b>Manage Account Details/Credentials</b></h4>
+            </div>
+            <div class="card-body">
+                <div class="container-fluid">
+                    <form id="register-frm" action="" method="post">
+                        <input type="hidden" name="id" value="<?= isset($id) ? $id : "" ?>">
+                        <div class="row">
+                            <div class="form-group col-md-6">
+                                <input type="text" name="firstname" id="firstname" placeholder="Enter First Name" autofocus class="form-control form-control-sm form-control-border" value="<?= isset($firstname) ? $firstname : "" ?>" required>
+                                <small class="ml-3">First Name</small>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <input type="text" name="middlename" id="middlename" placeholder="Enter Middle Name (optional)" class="form-control form-control-sm form-control-border" value="<?= isset($middlename) ? $middlename : "" ?>">
+                                <small class="ml-3">Middle Name</small>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <input type="text" name="lastname" id="lastname" placeholder="Enter Last Name" class="form-control form-control-sm form-control-border" required value="<?= isset($lastname) ? $lastname : "" ?>">
+                                <small class="ml-3">Last Name</small>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-md-6">
+                                <select name="gender" id="gender" class="custom-select custom-select-sm form-control-border" required>
+                                    <option <?= isset($gender) && $gender == 'Male' ? "selected" : "" ?>>Male</option>
+                                    <option <?= isset($gender) && $gender == 'Female' ? "selected" : "" ?>>Female</option>
+                                </select>
+                                <small class="ml-3">Gender</small>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <input type="text" name="contact" id="contact" placeholder="Enter Contact #" class="form-control form-control-sm form-control-border" required value="<?= isset($contact) ? $contact : "" ?>">
+                                <small class="ml-3">Contact #</small>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-md-12">
+                            <small class="ml-3">Address</small>
+                            <textarea name="address" id="address" rows="3" class="form-control form-control-sm rounded-0" placeholder="Block 6 Lot 23, Here Subd., There City, Anywhere, 2306"><?= isset($address) ? $address : "" ?></textarea>
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="row">
+                            <div class="form-group col-md-6">
+                                <input type="email" name="email" id="email" placeholder="jsmith@sample.com" class="form-control form-control-sm form-control-border" required value="<?= isset($email) ? $email : "" ?>">
+                                <small class="ml-3">Email</small>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-md-6">
+                                <div class="input-group">
+                                <input type="password" name="password" id="password" placeholder="" class="form-control form-control-sm form-control-border">
+                                <div class="input-group-append border-bottom border-top-0 border-left-0 border-right-0">
+                                    <span class="input-append-text text-sm"><i class="fa fa-eye-slash text-muted pass_type" data-type="password"></i></span>
+                                </div>
+                                </div>
+                                <small class="ml-3">New Password</small>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <div class="input-group">
+                                <input type="password" id="cpassword" placeholder="" class="form-control form-control-sm form-control-border">
+                                <div class="input-group-append border-bottom border-top-0 border-left-0 border-right-0">
+                                    <span class="input-append-text text-sm"><i class="fa fa-eye-slash text-muted pass_type" data-type="password"></i></span>
+                                </div>
+                                </div>
+                                <small class="ml-3">Confirm New Password</small>
+                            </div>
+                            <div class="col-12"><small class="text-muted"><em>Fill the password fields above only if you want to update your password.</em></small></div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-md-6">
+                                <div class="input-group">
+                                <input type="password" name="oldpassword" id="oldpassword" placeholder="" class="form-control form-control-sm form-control-border" required>
+                                <div class="input-group-append border-bottom border-top-0 border-left-0 border-right-0">
+                                    <span class="input-append-text text-sm"><i class="fa fa-eye-slash text-muted pass_type" data-type="password"></i></span>
+                                </div>
+                                </div>
+                                <small class="ml-3">Current Password</small>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-md-6">
+                            <label for="" class="control-label">Avatar</label>
+                            <div class="custom-file">
+                                    <input type="file" class="custom-file-input rounded-0 form-control form-control-sm form-control-border" id="customFile" name="img" onchange="displayImg(this,$(this))">
+                                    <label class="custom-file-label" for="customFile">Choose file</label>
+                                    </div>
+                            </div>
+                        <div class="row">
+                        </div>
+                            <div class="form-group col-md-6 d-flex justify-content-center">
+                            <img src="<?php echo validate_image(isset($image_path) ? $image_path : "") ?>" alt="" id="cimg" class="img-fluid img-thumbnail">
+                            </div>
+                        </div>
+                        <div class="row align-items-center">
+                            <div class="col-8">
+                            </div>
+                            <!-- /.col -->
+                            <div class="col-4">
+                            <button type="submit" class="btn btn-primary btn-sm btn-flat btn-block">Update Details</button>
+                            </div>
+                            <!-- /.col -->
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
