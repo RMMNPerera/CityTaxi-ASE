e3cc89ef9dbb122628d91313689422413470f6db
Thimedha Viraj <153438859+Thimedha@users.noreply.github.com>
2024-03-05 22:46:00 +0530

    php
    php code done

--- view_booking.php
+++ view_booking.php
@@ -1,3 +1,21 @@
+<?php
+require_once('./config.php');
+if(isset($_GET['id']) && $_GET['id'] > 0){
+    $qry = $conn->query("SELECT * from `booking_list` where id = '{$_GET['id']}' ");
+    if($qry->num_rows > 0){
+        foreach($qry->fetch_assoc() as $k => $v){
+            $$k=$v;
+        }
+        $qry2 = $conn->query("SELECT c.*, cc.name as category from `cab_list` c inner join category_list cc on c.category_id = cc.id where c.id = '{$cab_id}' ");
+        if($qry2->num_rows > 0){
+            foreach($qry2->fetch_assoc() as $k => $v){
+                if(!isset($$k))
+                $$k=$v;
+            }
+        }
+    }
+}
+?>
 <style>
     #uni_modal .modal-footer{
         display:none
@@ -11,17 +29,17 @@
                 <legend class="h5 text-muted"> Cab Details</legend>
                 <dl>
                     <dt class="">Cab Body No</dt>
-                    <dd class="pl-4"></dd>
+                    <dd class="pl-4"><?= isset($body_no) ? $body_no : "" ?></dd>
                     <dt class="">Vehicle Category</dt>
-                    <dd class="pl-4"></dd>
+                    <dd class="pl-4"><?= isset($category) ? $category : "" ?></dd>
                     <dt class="">Vehicle model</dt>
-                    <dd class="pl-4"></dd>
+                    <dd class="pl-4"><?= isset($cab_model) ? $cab_model : "" ?></dd>
                     <dt class="">Driver</dt>
-                    <dd class="pl-4"></dd>
+                    <dd class="pl-4"><?= isset($cab_driver) ? $cab_driver : "" ?></dd>
                     <dt class="">Driver Contact</dt>
-                    <dd class="pl-4"></dd>
+                    <dd class="pl-4"><?= isset($driver_contact) ? $driver_contact : "" ?></dd>
                     <dt class="">Driver Address</dt>
-                    <dd class="pl-4"></dd>
+                    <dd class="pl-4"><?= isset($driver_address) ? $driver_address : "" ?></dd>
                 </dl>
             </fieldset>
         
@@ -33,11 +51,11 @@
                 <legend class="h5 text-muted"> Booking Details</legend>
                 <dl>
                     <dt class="">Ref. Code</dt>
-                    <dd class="pl-4"></dd>
+                    <dd class="pl-4"><?= isset($ref_code) ? $ref_code : "" ?></dd>
                     <dt class="">Pickup Zone</dt>
-                    <dd class="pl-4"></dd>
+                    <dd class="pl-4"><?= isset($pickup_zone) ? $pickup_zone : "" ?></dd>
                     <dt class="">Drop off Zone</dt>
-                    <dd class="pl-4"></dd>
+                    <dd class="pl-4"><?= isset($drop_zone) ? $drop_zone : "" ?></dd>
                     <dt class="">Status</dt>
                     <dd class="pl-4">
                         <?php 
@@ -69,14 +87,16 @@
     
     <div class="text-right">
         
+        <?php if(isset($status) && $status == 0): ?>
         <button class="btn btn-danger btn-flat bg-gradient-danger" type="button" id="cancel_booking">Cancel Bookings</button>
+        <?php endif; ?>
         <button class="btn btn-dark btn-flat bg-gradient-dark" type="button" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>
     </div>
 </div>
 <script>
     $(function(){
         $('#cancel_booking').click(function(){
-            _conf("Are you sure to cancel your cab booking [Ref. Code: <b></b>]?", "cancel_booking",[""])
+            _conf("Are you sure to cancel your cab booking [Ref. Code: <b><?= isset($ref_code) ? $ref_code : "" ?></b>]?", "cancel_booking",["<?= isset($id) ? $id : "" ?>"])
         })
     })
     function cancel_booking($id){
