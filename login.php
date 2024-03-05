f339e1801272066a4b55c2fba10db0ad3f35e33f
VishmiRamona <155454191+VishmiRamona@users.noreply.github.com>
2024-03-06 01:07:21 +0530

    Completed js code
    Script and j query link to loading

--- login.php
+++ login.php
@@ -3,6 +3,9 @@
 <html lang="en" class="" style="height: auto;">
  <?php require_once('inc/header.php') ?>
 <body class="hold-transition login-page">
+  <script>
+    start_loader()
+  </script>
   <style>
       body{
           /* width:calc(100%);
@@ -72,6 +75,17 @@
 </div>
 <!-- /.login-box -->
 
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
 
+<script>
+  $(document).ready(function(){
+    end_loader();
+  })
+</script>
 </body>
 </html>
