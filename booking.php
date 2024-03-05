558a7a2e7288e1b1791bbfc6be164dc71a1a6977
charithanayana <charithanayana@gmail.com>
2024-03-05 12:02:34 +0530

    all functions done and included script

--- booking.php
+++ booking.php
@@ -12,4 +12,39 @@
         </div>
     </form>
 </div>
+<script>
+	$(document).ready(function(){
+		$('#booking-form').submit(function(e){
+			e.preventDefault();
+            var _this = $(this)
+			 $('.err-msg').remove();
+			start_loader();
+			$.ajax({
+				url:_base_url_+"classes/Master.php?f=save_booking",
+				data: new FormData($(this)[0]),
+				error:err=>{
+					console.log(err)
+					alert_toast("An error occured",'error');
+					end_loader();
+				},
+				success:function(resp){
+					if(typeof resp =='object' && resp.status == 'success'){
+						location.href = './?p=booking_list';
+					}else if(resp.status == 'failed' && !!resp.msg){
+                        var el = $('<div>')
+                            el.addClass("alert alert-danger err-msg").text(resp.msg)
+                            _this.prepend(el)
+                            el.show('slow')
+                            $("html, body").animate({ scrollTop: _this.closest('.card').offset().top }, "fast");
+                            end_loader()
+                    }else{
+						alert_toast("An error occured",'error');
+						end_loader();
+                        console.log(resp)
+					}
+				}
+			})
+		})
+	})
+</script>
 
