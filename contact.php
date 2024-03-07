17927290897c224b83139363c41cf0ebce72d90d
VishmiRamona <155454191+VishmiRamona@users.noreply.github.com>
2024-03-07 12:27:22 +0530

    Completed contact Js code
    Js script coding

--- contact.php
+++ contact.php
@@ -1,5 +1,5 @@
  <!-- Header-->
- <header class="bg-dark py-5" id="main-header">
+<header class="bg-dark py-5" id="main-header">
     <div class="container h-100 d-flex align-items-end justify-content-center w-100">
         <div class="text-center text-white w-100">
             <h1 class="display-4 fw-bolder mx-5">About Us</h1>
@@ -17,3 +17,17 @@
         </div>
     </div>
 </section>
+
+<script>
+    $(document).scroll(function() { 
+        $('#topNavBar').removeClass('bg-purple navbar-light navbar-dark bg-gradient-purple text-light')
+        if($(window).scrollTop() === 0) {
+           $('#topNavBar').addClass('navbar-dark bg-purple text-light')
+        }else{
+           $('#topNavBar').addClass('navbar-dark bg-gradient-purple ')
+        }
+    });
+    $(function(){
+        $(document).trigger('scroll')
+    })
+</script>
