a2715c47b24299da5ee51e6e6d5c3aea7eeb3dff
saarah zainab <155457563+saarah-zainab@users.noreply.github.com>
2024-03-07 10:43:13 +0530

    java script included done

--- register.php
+++ register.php
@@ -1,4 +1,7 @@
 <body class="">
+  <script>
+    start_loader()
+  </script>
   <style>
     html, body{
       width:calc(100%);
@@ -145,5 +148,82 @@
 <script src="<?= base_url ?>plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
 <!-- <script src="<?= base_url ?>dist/js/adminlte.min.js"></script> -->
+
+<script>
+  window.displayImg = function(input,_this) {
+	    if (input.files && input.files[0]) {
+	        var reader = new FileReader();
+	        reader.onload = function (e) {
+	        	$('#cimg').attr('src', e.target.result);
+	        	_this.siblings('.custom-file-label').html(input.files[0].name)
+	        }
+
+	        reader.readAsDataURL(input.files[0]);
+	    }else{
+            $('#cimg').attr('src', "<?php echo validate_image(isset($image_path) ? $image_path : "") ?>");
+            _this.siblings('.custom-file-label').html("Choose file")
+        }
+	}
+  $(document).ready(function(){
+    end_loader();
+    $('.pass_type').click(function(){
+      var type = $(this).attr('data-type')
+      if(type == 'password'){
+        $(this).attr('data-type','text')
+        $(this).closest('.input-group').find('input').attr('type',"text")
+        $(this).removeClass("fa-eye-slash")
+        $(this).addClass("fa-eye")
+      }else{
+        $(this).attr('data-type','password')
+        $(this).closest('.input-group').find('input').attr('type',"password")
+        $(this).removeClass("fa-eye")
+        $(this).addClass("fa-eye-slash")
+      }
+    })
+    $('#register-frm').submit(function(e){
+      e.preventDefault()
+      var _this = $(this)
+			 $('.err-msg').remove();
+       var el = $('<div>')
+            el.hide()
+      if($('#password').val() != $('#cpassword').val()){
+        el.addClass('alert alert-danger err-msg').text('Password does not match.');
+        _this.prepend(el)
+        el.show('slow')
+        return false;
+      }
+			start_loader();
+			$.ajax({
+				url:_base_url_+"classes/Users.php?f=save_client",
+				data: new FormData($(this)[0]),
+                cache: false,
+                contentType: false,
+                processData: false,
+                method: 'POST',
+                type: 'POST',
+                dataType: 'json',
+				error:err=>{
+					console.log(err)
+					alert_toast("An error occured",'error');
+					end_loader();
+				},
+				success:function(resp){
+					if(typeof resp =='object' && resp.status == 'success'){
+						location.href = "./login.php";
+					}else if(resp.status == 'failed' && !!resp.msg){   
+              el.addClass("alert alert-danger err-msg").text(resp.msg)
+              _this.prepend(el)
+              el.show('slow')
+          }else{
+						alert_toast("An error occured",'error');
+						end_loader();
+                        console.log(resp)
+					}
+          $('html, body').scrollTop(0)
+				}
+			})
+    })
+  })
+</script>
 </body>
 </html>
